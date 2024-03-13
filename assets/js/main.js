38e6702690ed5b733ba917bf8f91458f07d0f0be
Ni <120183605+anhquynhanh@users.noreply.github.com>
2024-03-13 17:51:46 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,4 @@
-const loginBtn = document.querySelector('.header-btn-login');
-const headerBtn = document.querySelector('.header-btn');
-const cont1Btn = document.querySelector('.container-1-btn-1');
-const cont2Btn = document.querySelector('.container-1-btn-2');
-const cont3Btn = document.querySelector('.btn-con-2');
-
-loginBtn.addEventListener('click', () => {
-    alert('Login successful');
-});
-
-headerBtn.addEventListener('click', () => {
-    alert('Started');
-});
-
-cont1Btn.addEventListener('click', () => {
-    alert('Started');
-});
-
-cont2Btn.addEventListener('click', () => {
-    alert('Learned');
-});
-
-cont3Btn.addEventListener('click', () => {
-    alert('Started');
+document.querySelector('.header--menu').addEventListener('click', function() {
+    document.querySelector('.header__links').style.display = 'flex';
+    document.querySelector('.header__btn').style.display = 'flex';
 });
