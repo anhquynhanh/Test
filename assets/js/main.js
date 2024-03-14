41d4f2eb681b54c0b1de2659f8e7139f3c3202ad
Ni <120183605+anhquynhanh@users.noreply.github.com>
2024-03-14 14:41:47 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 document.querySelector('.header--menu').addEventListener('click', function() {
-    document.querySelector('.header__links').style.display = 'flex';
-    document.querySelector('.header__btn').style.display = 'flex';
+    const header = document.querySelector('.header');
+    header.classList.toggle('header__menu--open');
+    if (header.classList.contains('header__menu--open')) {
+        header.querySelector('.header__links').style.display = 'none';
+        header.querySelector('.header__btn').style.display = 'none';
+    } else {
+        header.querySelector('.header__links').style.display = 'flex';
+        header.querySelector('.header__btn').style.display = 'flex';
+    }
 });
